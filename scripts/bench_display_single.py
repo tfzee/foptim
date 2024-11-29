e89e276e86b828dab578461d737e2616904ff643
tfzee <timziegler99@gmail.com>
2024-11-29 23:29:39 +0100

    changed arround the scripts

--- scripts/bench_display_single.py
+++ scripts/bench_display_single.py
@@ -25,7 +25,7 @@ def show_plot_comp(filename, filename2):
     data = pd.read_csv(filename)
     data2 = pd.read_csv(filename2)
 
-    colors = [("red" if x.endswith("baseline") else "green") for x in data["command"] if any([x.startswith(start) for start in tests_to_show])]
+    colors = [("red" if x.endswith("baseline") else "green") for x in data["command"]]
     
     plt.bar(data["command"], np.array(data2["mean"])/np.array(data["mean"]), color = colors)
     plt.show()
@@ -36,4 +36,5 @@ def show_plot_comp(filename, filename2):
 
 if __name__ == "__main__":
     show_plot_single("../build/test/Output/perf.csv")
-    show_plot_single("../build/test/Output/compile.csv")
+    show_plot_comp("../build/test/Output/perf_old.csv", "../build/test/Output/perf.csv")
+    # show_plot_single("../build/test/Output/compile.csv")
--- scripts/bench_record.py
+++ scripts/bench_record.py
@@ -6,22 +6,18 @@ tests_to_record = ["matmul.cpp", "prime_sieve.c", "fib.c"]
 if __name__ == "__main__":
     out_dir = "../build/test/Output"
 
-    if collect_compiletimes:
-        compile_command = f"hyperfine -N --export-csv={out_dir}/compile.csv"
-    else:
-        compile_command = f"clang --version"
-
     benches = [benchy for benchy in os.listdir("../test/") if(benchy.endswith(".c") or benchy.endswith(".cpp"))]
     benches = [bench for bench in benches if bench in tests_to_record];
 
     clang_options = "-ffreestanding -fno-exceptions -fno-stack-protector"
     
     
-    for benchy in benches:
-        if collect_compiletimes:
+    if collect_compiletimes:
+        compile_command = f"hyperfine -N --export-csv={out_dir}/compile.csv"
+        for benchy in benches:
             clang_command = f"clang{'++' if benchy.endswith(".cpp") else ''} -O0 -Xclang -disable-O0-optnone {clang_options} ../test/{benchy} -o {out_dir}/{benchy}.tmp.ll -S -emit-llvm"
             os.system(clang_command)
-        
+    
             name = ".".join(benchy.split(".")[:-1])
             compile_command += f" -n {name}_compile"
             compile_command += f" \"../build/foptim_main {out_dir}/{benchy}.tmp.ll {out_dir}/{benchy}.tmp.ss\""
@@ -31,17 +27,18 @@ if __name__ == "__main__":
 
             compile_command += f" -n {name}_clang_O3_compile_baseline"
             compile_command += f" \"clang++ {out_dir}/{benchy}.tmp.ll -march=native -O3 {clang_options} -o {out_dir}/{benchy}_clang_O3.tmp.out\""
-        else:
-            compile_command += f" && "
-            compile_command += f" ../build/foptim_main {out_dir}/{benchy}.tmp.ll {out_dir}/{benchy}.tmp.ss"
-            compile_command += f" && "
-            compile_command += f" clang++ {out_dir}/{benchy}.tmp.ll -march=native -O1 {clang_options} -o {out_dir}/{benchy}_clang_O1.tmp.out"
-            compile_command += f" && "
-            compile_command += f" clang++ {out_dir}/{benchy}.tmp.ll -march=native -O3 {clang_options} -o {out_dir}/{benchy}_clang_O3.tmp.out"
+        os.system(compile_command)  
+    else:
+        for benchy in benches:
+            clang_command = f"clang{'++' if benchy.endswith(".cpp") else ''} -O0 -Xclang -disable-O0-optnone {clang_options} ../test/{benchy} -o {out_dir}/{benchy}.tmp.ll -S -emit-llvm"
+            os.system(clang_command)
+            os.system(f"../build/foptim_main {out_dir}/{benchy}.tmp.ll {out_dir}/{benchy}.tmp.ss")
+            os.system(f"clang++ {out_dir}/{benchy}.tmp.ll -march=native -O1 {clang_options} -o {out_dir}/{benchy}_clang_O1.tmp.out")
+            os.system(f"clang++ {out_dir}/{benchy}.tmp.ll -march=native -O3 {clang_options} -o {out_dir}/{benchy}_clang_O3.tmp.out")
+            print(f"clang++ {out_dir}/{benchy}.tmp.ll -march=native -O3 {clang_options} -o {out_dir}/{benchy}_clang_O3.tmp.out")
             
 
     # print(hyperfine_compile_command)
-    os.system(compile_command)  
 
     hyperfine_run_command = f"hyperfine -i -N --export-csv={out_dir}/perf.csv"
     for benchy in benches:
