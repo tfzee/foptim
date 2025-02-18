84f7a942919c141aef711f2a6afb011de46ec6e4
tfzee <timziegler99@gmail.com>
2025-02-18 14:14:02 +0100

    Also comparing gcc now in minimizer

--- scripts/minimize_output.sh
+++ scripts/minimize_output.sh
@@ -7,20 +7,35 @@ foptim="$build_dir/foptim_main"
 
 clang++ -O0 $test_file -o min.ll -S -emit-llvm || exit 1
 clang++ -O1 $test_file -Werror=return-type -Werror=uninitialized -Wall -Wextra -o clang_min.out || exit 1
+g++ -O1 $test_file -Werror=return-type -Werror=uninitialized -Wall -Wextra -o gcc_min.out || exit 1
 
 $foptim min.ll min.o || exit 0
 clang++ min.o -o min.out -lm || exit 1
 
+echo "Running Mine"
 ./min.out
 stats_got=$?
+echo "Rnning Clang"
 ./clang_min.out
 stats_exp=$?
+echo "Rnning GCC"
+./gcc_min.out
+stats_exp2=$?
+echo "Exit Mine"
 echo $stats_got
+echo "Exit Clang"
 echo $stats_exp
+echo "Exit Gcc"
+echo $stats_exp2
 
-if [[ $stats_got != $stats_exp ]] && [[ $stats_exp != 132 ]]; then
+if [[ $stats_got != $stats_exp ]]; then
+if [[ $stats_exp != $stats_exp2 ]]; then
+  echo "Bad Failed"
+  exit 1
+else
   echo "Good Failed"
   exit 0
+fi
 else
   exit 1
 fi
