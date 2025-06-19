227e3ff2e927fb58a842df8402c0fc79b12e14ac
Tim <timziegler99@gmail.com>
2025-06-19 17:34:56 +0200

    Added func annotation pass that annoates mem beheaviour for now

---
+++ src/optim/func_passes/func_annotator.hpp
@@ -0,0 +1,62 @@
+#pragma once
+#include "ir/function.hpp"
+#include "ir/instruction_data.hpp"
+#include "optim/function_pass.hpp"
+#include "utils/bitset.hpp"
+#include <fmt/core.h>
+
+namespace foptim::optim {
+
+class FuncAnnotator final : public FunctionPass {
+public:
+  void apply(fir::Context & /*ctx*/, fir::Function &func) override {
+    bool does_read = false;
+    bool does_write = false;
+
+    for (auto bb : func.basic_blocks) {
+      for (auto instr : bb->instructions) {
+        if (instr->is(fir::InstrType::StoreInstr)) {
+          does_write = true;
+        }
+        if (instr->is(fir::InstrType::LoadInstr)) {
+          does_read = true;
+        }
+        if (instr->is(fir::InstrType::CallInstr)) {
+          if (instr->args[0].is_constant() &&
+              instr->args[0].as_constant()->is_func()) {
+            auto f = instr->args[0].as_constant()->as_func();
+            if (f->mem_read_none) {
+            } else if (f->mem_read_only) {
+              does_read = true;
+            } else if (f->mem_write_only) {
+              does_write = true;
+            } else {
+              does_read = true;
+              does_write = true;
+            }
+          } else {
+            does_read = true;
+            does_write = true;
+          }
+        }
+        if (does_read && does_write) {
+          break;
+        }
+      }
+      if (does_read && does_write) {
+        break;
+      }
+    }
+    func.mem_read_none = false;
+    func.mem_read_only = false;
+    func.mem_write_only = false;
+    if (does_read && !does_write) {
+      func.mem_read_only = true;
+    } else if (!does_read && does_write) {
+      func.mem_write_only = true;
+    } else if (!does_read && !does_write) {
+      func.mem_read_none = true;
+    }
+  }
+};
+} // namespace foptim::optim
