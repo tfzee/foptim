072d56a321b6d70c9d0814915fd29a3ca75e5bf2
Tim <timziegler99@gmail.com>
2025-06-20 15:32:51 +0200

    Added no recurse to func annotator

--- src/optim/func_passes/func_annotator.hpp
+++ src/optim/func_passes/func_annotator.hpp
@@ -2,7 +2,6 @@
 #include "ir/function.hpp"
 #include "ir/instruction_data.hpp"
 #include "optim/function_pass.hpp"
-#include "utils/bitset.hpp"
 #include <fmt/core.h>
 
 namespace foptim::optim {
@@ -11,6 +10,7 @@ class FuncAnnotator final : public FunctionPass {
 public:
   void apply(fir::Context & /*ctx*/, fir::Function &func) override {
     bool does_read = false;
+    bool wont_recurse = true;
     bool does_write = false;
 
     for (auto bb : func.basic_blocks) {
@@ -25,6 +25,9 @@ public:
           if (instr->args[0].is_constant() &&
               instr->args[0].as_constant()->is_func()) {
             auto f = instr->args[0].as_constant()->as_func();
+            if (!f->no_recurse) {
+              wont_recurse = false;
+            }
             if (f->mem_read_none) {
             } else if (f->mem_read_only) {
               does_read = true;
@@ -35,21 +38,27 @@ public:
               does_write = true;
             }
           } else {
+            wont_recurse = false;
             does_read = true;
             does_write = true;
           }
         }
-        if (does_read && does_write) {
+        if (does_read && does_write && !wont_recurse) {
           break;
         }
       }
-      if (does_read && does_write) {
+      if (does_read && does_write && !wont_recurse) {
         break;
       }
     }
     func.mem_read_none = false;
     func.mem_read_only = false;
     func.mem_write_only = false;
+    if (wont_recurse) {
+      // dont reset this one since a no recurse function wont be turned into
+      // recurse in our stuff atleast for now
+      func.no_recurse = true;
+    }
     if (does_read && !does_write) {
       func.mem_read_only = true;
     } else if (!does_read && does_write) {
