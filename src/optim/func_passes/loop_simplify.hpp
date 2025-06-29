a3b4b98d119f090eca53a905abed489df8ad17d1
tfzee <timziegler99@gmail.com>
2025-06-29 12:02:28 +0200

    Added dead loop elimination

--- src/optim/func_passes/loop_simplify.hpp
+++ src/optim/func_passes/loop_simplify.hpp
@@ -1,11 +1,14 @@
 #pragma once
+#include "ir/builder.hpp"
 #include "ir/function.hpp"
 #include "ir/instruction_data.hpp"
+#include "ir/use.hpp"
 #include "optim/analysis/cfg.hpp"
 #include "optim/analysis/dominators.hpp"
 #include "optim/analysis/loop_analysis.hpp"
 #include "optim/function_pass.hpp"
 #include "utils/bitset.hpp"
+#include <algorithm>
 
 namespace foptim::optim {
 
@@ -17,89 +20,73 @@ public:
     Dominators dom{cfg};
     LoopInfoAnalysis loops{dom};
 
-    for (auto loop : loops.info) {
-      InductionVarAnalysis induct{cfg, loop};
-
-      bool failed = false;
-      fir::Instr v{fir::Instr::invalid()};
-
-      for (auto leaving : loop.leaving_nodes) {
-        auto term = cfg.bbrs[leaving].bb->get_terminator();
-        ASSERT(term->is(fir::InstrType::CondBranchInstr));
-        ASSERT(term->args[0].is_instr());
-        auto cond = term->args[0].as_instr();
-        if (!cond->is(fir::InstrType::ICmp)) {
-          failed = true;
+    size_t loop_ip1 = 1;
+    // eliminate all loops that dont have sideffects
+    //  TODO: handle if its values are used after and their result value is
+    //  known/can be calculated withuot loop
+    for (; loop_ip1 <= loops.info.size(); loop_ip1++) {
+      auto &loop = loops.info.at(loop_ip1 - 1);
+      bool has_sideeffects = false;
+      TVec<fir::Use> use_after;
+      for (auto b : loop.body_nodes) {
+        if (has_sideeffects) {
           break;
         }
-        if (!cond->args[0].is_instr() || !cond->args[1].is_constant()) {
-          failed = true;
-          break;
+        for (auto arg : cfg.bbrs[b].bb->args) {
+          for (auto u : arg->uses) {
+            auto use_bb = cfg.get_bb_id(u.user->get_parent());
+            if (u.type == fir::UseType::BBArg) {
+              if (!std::ranges::contains(
+                      loop.body_nodes,
+                      cfg.get_bb_id(u.user->bbs[u.argId].bb))) {
+                use_after.push_back(u);
+              }
+            } else if (!std::ranges::contains(loop.body_nodes, use_bb)) {
+              use_after.push_back(u);
+            }
+          }
         }
-        if (!v.is_valid()) {
-          v = cond;
-        } else if (v != cond) {
-          failed = true;
-          break;
-        }
-      }
-      if (failed) {
-        continue;
-      }
-      fmt::println("{}", func);
-      auto cond_cond = (fir::ICmpInstrSubType)v->subtype;
-      auto cond_input = v->args[0].as_instr();
-      auto cond_bound = v->args[1].as_constant();
-      bool incrementing;
-      bool found = false;
-      for (auto &var : induct.direct_inductvars) {
-        incrementing =
-            var.type == InductionVarAnalysis::IterationType::PlusConst;
-        if (var.def == fir::ValueR{cond_input} && var.consti->is_int() &&
-            (incrementing ||
-             var.type == InductionVarAnalysis::IterationType::SubConst)) {
-          auto v = var.consti->as_int();
-          if (v == 1) {
-            continue;
-          } else if (v == -1) {
-            incrementing = !incrementing;
-          } else {
+        for (auto i : cfg.bbrs[b].bb->instructions) {
+          if (i->has_pot_sideeffects()) {
+            has_sideeffects = true;
             break;
           }
-          found = true;
-          break;
-        }
-      }
-      for (auto &var : induct.indirect_inductvars) {
-        incrementing =
-            var.type == InductionVarAnalysis::IterationType::PlusConst;
-        if (var.def == fir::ValueR{cond_input} &&
-            (incrementing ||
-             var.type == InductionVarAnalysis::IterationType::SubConst)) {
-          auto v = var.arg2.as_constant()->as_int();
-          if (v == 1) {
-            continue;
-          } else if (v == -1) {
-            incrementing = !incrementing;
-          } else {
-            break;
+          for (auto u : i->uses) {
+            auto use_bb = cfg.get_bb_id(u.user->get_parent());
+            if (u.type == fir::UseType::BBArg) {
+              if (!std::ranges::contains(
+                      loop.body_nodes,
+                      cfg.get_bb_id(u.user->bbs[u.argId].bb))) {
+                use_after.push_back(u);
+              }
+            } else if (!std::ranges::contains(loop.body_nodes, use_bb)) {
+              use_after.push_back(u);
+            }
           }
-          found = true;
-          break;
         }
       }
-      if (!found || failed) {
+      if (!has_sideeffects && use_after.empty()) {
+        auto head_term = cfg.bbrs[loop.head].bb->get_terminator();
+        ASSERT(head_term->is(fir::InstrType::CondBranchInstr))
+        fir::Builder bb{head_term};
+        auto leave_index = 0;
+        if (std::ranges::contains(loop.body_nodes,
+                                  cfg.get_bb_id(head_term->bbs[0].bb))) {
+          leave_index = 1;
+        }
+        auto new_branch = bb.build_branch(head_term->bbs[leave_index].bb);
+        for (auto a : head_term->bbs[leave_index].args) {
+          new_branch.add_bb_arg(0, a);
+        }
+        fmt::println("{} {}", head_term, leave_index);
+        head_term.destroy();
+        fmt::println("{}", func);
+        cfg.update(func, false);
+        dom.update(cfg);
+        loops.update(dom);
+        loop_ip1 = 0;
         continue;
       }
-
-      (void)cond_cond;
-      fmt::println("{}", v);
-      fmt::println("{}", cond_input);
-      fmt::println("{}", cond_bound);
-      fmt::println("{}", incrementing);
-      fmt::println("{}", v);
-
-      induct.dump();
     }
   }
 };
