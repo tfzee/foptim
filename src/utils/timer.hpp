da6c147adf85f98051e821da48febbda9fec550d
tfzee <timziegler99@gmail.com>
2025-07-13 17:28:10 +0200

    Added a timer to get a overview

---
+++ src/utils/timer.hpp
@@ -0,0 +1,48 @@
+#pragma once
+#include "utils/types.hpp"
+#include <chrono>
+#include <fmt/core.h>
+#include <unordered_map>
+
+namespace foptim::utils {
+
+struct ScopedTimer;
+
+struct DumbTimer {
+  std::unordered_map<const char *, u64> times;
+
+  void print() {
+    for (const auto &[name, time] : times) {
+      if (time > 100'000'000) {
+        fmt::println("{:<20} {:>10.3f}  s", name, (double)time / 1'000'000'000);
+      } else if (time > 10'000) {
+        fmt::println("{:<20} {:>10.3f} ms", name, (double)time / 1'000'000);
+      } else {
+        fmt::println("{:<20} {:>6}     ns", name, time);
+      }
+    }
+  }
+
+  ScopedTimer scopedTimer(const char *name);
+};
+
+struct ScopedTimer {
+  DumbTimer *timer;
+  const char *name;
+  std::chrono::high_resolution_clock::time_point start;
+
+  ScopedTimer(DumbTimer *t, const char *name)
+      : timer(t), name(name), start(std::chrono::high_resolution_clock::now()) {
+  }
+
+  ~ScopedTimer() {
+    auto end = std::chrono::high_resolution_clock::now();
+    auto time =
+        std::chrono::duration_cast<std::chrono::nanoseconds>(end - start)
+            .count();
+    timer->times.insert({name, time});
+  }
+};
+
+ScopedTimer DumbTimer::scopedTimer(const char *name) { return {this, name}; }
+} // namespace foptim::utils
