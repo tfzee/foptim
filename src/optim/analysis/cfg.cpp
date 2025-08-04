329c6b833897227a2062a52a6586d7c156a4b226
tfzee <timziegler99@gmail.com>
2025-08-04 13:51:35 +0200

    reimpled cfg dumping

--- src/optim/analysis/cfg.cpp
+++ src/optim/analysis/cfg.cpp
@@ -3,20 +3,30 @@
 namespace foptim::optim {
 
 void CFG::dump() const {
-  TODO("reimpl");
-  // print << "DUMP CFG\n";
+  fmt::println("DUMP CFG");
 
   // for (const auto &node : bbrs) {
-  //   print << "BB: " << node.bb.get_raw_ptr() << "\n  SUCC:\n";
+  //   fmt::println("BB: {}\n SUCC:", (void *)node.bb.get_raw_ptr());
   //   for (const auto &succ : node.succ) {
-  //     print << "    " << bbrs[succ].bb.get_raw_ptr() << "\n";
+  //     fmt::println("    {}", (void *)bbrs[succ].bb.get_raw_ptr());
   //   }
-  //   print << "  PRED:\n";
+  //   fmt::println(" PRED:");
   //   for (const auto &pred : node.pred) {
-  //     print << "   " << bbrs[pred].bb.get_raw_ptr() << "\n";
+  //     fmt::println("    {}", (void *)bbrs[pred].bb.get_raw_ptr());
   //   }
   // }
+  for (size_t node_id = 0; node_id < bbrs.size(); node_id++) {
+    fmt::println("BB: {}\n SUCC:", node_id);
+    for (const auto &succ : bbrs[node_id].succ) {
+      fmt::println("    {}", succ);
+    }
+    fmt::println(" PRED:");
+    for (const auto &pred : bbrs[node_id].pred) {
+      fmt::println("    {}", pred);
+    }
+  }
 }
+
 void CFG::dump_graph() const {
   TODO("reimpl");
   // print << "=================================================0\n";
