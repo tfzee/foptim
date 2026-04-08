ec90bd2bd9e1938536cd57f47fc5043115d194ae
tfzee <timziegler99@gmail.com>
2026-04-08 15:41:43 +0200

    Cleanup in the minize thingies so they actually work good with newest setup

--- scripts/minimize.sh
+++ scripts/minimize.sh
@@ -1,16 +1,19 @@
 #!/usr/bin/env bash 
 
-build_dir="/home/tim/programming/foptim/build"
-test_file="/home/tim/programming/foptim/test/min.c"
+TEST_FOLDER="$HOME/programming/foptim_test"
+build_dir="$HOME/programming/foptim/build"
+test_file="min.cpp"
 
-failure_string="Instr has invalid number of basicblock arguments"
+failure_string="Cant have a isntruction referencing itself"
 
+flags="-U__SIZEOF_INT128__ -std=c++26 -fno-stack-protector"
+test_linkdir="-I$TEST_FOLDER/test/CppPerformanceBenchmarks/ -I$TEST_FOLDER/test/embench/"
 
 foptim="$build_dir/foptim_main"
 
-clang -O0 "$test_file" -o "min.ll" -S -emit-llvm || exit 1
+clang $flags $test_linkdir -O0 $test_file -o min.ll -S -emit-llvm || exit 1
 
-compile_output=$($foptim "min.ll" "min.ss" 2>&1)
+compile_output=$($foptim min.ll min.o 2>&1)
 echo "$compile_output"
 if [[ $compile_output == *"$failure_string"* ]]; then
   exit 0
--- scripts/minimize_output.sh
+++ scripts/minimize_output.sh
@@ -7,15 +7,16 @@ BUILD_DIR="$FOLDER/build"
 
 test_file="min.cpp"
 foptim="$BUILD_DIR/foptim_main"
-flags="-U__SIZEOF_INT128__ -std=c++26 -fno-exceptions -fno-stack-protector"
-# flags="-U__SIZEOF_INT128__ -std=c++26 -fno-stack-protector"
+# flags="-U__SIZEOF_INT128__ -std=c++26 -fno-exceptions -fno-stack-protector"
+flags="-U__SIZEOF_INT128__ -std=c++26 -fno-stack-protector"
+# flags=""
 test_linkdir="-I$TEST_FOLDER/test/CppPerformanceBenchmarks/ -I$TEST_FOLDER/test/embench/"
 # compile_optim="-O3 -mllvm -disable-llvm-optzns"
 compile_optim="-O0"
 
 clang++ $compile_optim $flags $test_linkdir $test_file -o min.ll -S -emit-llvm || exit 1
 clang++ -static-libstdc++ -O3 $flags $test_linkdir $test_file -Werror=return-type -Werror=uninitialized -Wall -Wextra -o clang_min.out || exit 1
-g++ -static-libstdc++ -O0 $flags $test_linkdir $test_file -Werror=return-type -Werror=uninitialized -Wall -Wextra -o gcc_min.out || exit 1
+g++ -static-libstdc++ -O0 $flags $test_linkdir $test_file -Werror=return-type -Werror=uninitialized -fsanitize=undefined -Wall -Wextra -o gcc_min.out || exit 1
 
 $foptim min.ll min.o || exit 0
 clang++ min.o -o min.out -static-libstdc++ || exit 1
@@ -37,10 +38,10 @@ echo $stats_exp
 echo "$OUT_exp2"
 echo $stats_exp2
 
+# if [[ "$stats_exp" != "$stats_exp2" ]] || [[ "$OUT_exp" != "$OUT_exp2" ]] || [[ "$OUT_exp" != *"2450.000000"* ]] || [[ "$stats_exp" != "0" ]]; then
+# if [[ "$stats_exp" != "$stats_exp2" ]] || [[ "$OUT_exp" != "$OUT_exp2" ]] || [[ "$stats_exp" != "0" ]]; then
 if [[ "$stats_got" != "$stats_exp" ]] || [[ "$OUT_got" != "$OUT_exp" ]]; then
 if [[ "$stats_exp" != "$stats_exp2" ]] || [[ "$OUT_exp" != "$OUT_exp2" ]] || [[ "$stats_exp" != "0" ]]; then
-# if [[ "$stats_got" != "$stats_exp" ]]; then
-# if [[ "$stats_exp" != "$stats_exp2" ]]; then
   echo "Bad Failed"
   exit 1
 else
